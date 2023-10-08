04452a53d2fd2c0472a86aa098eafc0031486566
George Psomathianos <45467851+GeorgePsom@users.noreply.github.com>
2023-10-08 10:30:43 +0100

    New clean project

--- source/pch.h
+++ source/pch.h
@@ -27,26 +27,26 @@
 #include <map>
 
 // Include core headers
-#include "vulkan_api/Debugger.h"
-#include "vulkan_api/Instance.h"
-#include "platform/Surface.h"
-#include "platform/windows/WindowSurface.h"
-#include "vulkan_api/PhysicalDevice.h"
-#include "vulkan_api/Device.h"
-#include "vulkan_api/Swapchain.h"
-#include "vulkan_api/Shader.h"
-#include "vulkan_api/Vertex.h"
-#include "vulkan_api/RenderPass.h"
-#include "vulkan_api/Pipeline.h"
-#include "vulkan_api/Framebuffer.h"
-
-#include "vulkan_api/VertexBuffer.h"
-
-#include "vulkan_api/CommandPool.h"
-#include "vulkan_api/CommandBuffer.h"
-
-#include "vulkan_api/synchronization/Fence.h"
-#include "vulkan_api/synchronization/Semaphore.h"
-#include "vulkan_api/synchronization/Presenter.h"
-
-#include "Application.h"
+//#include "vulkan_api/Debugger.h"
+//#include "vulkan_api/Instance.h"
+//#include "platform/Surface.h"
+//#include "platform/windows/WindowSurface.h"
+//#include "vulkan_api/PhysicalDevice.h"
+//#include "vulkan_api/Device.h"
+//#include "vulkan_api/Swapchain.h"
+//#include "vulkan_api/Shader.h"
+//#include "vulkan_api/Vertex.h"
+//#include "vulkan_api/RenderPass.h"
+//#include "vulkan_api/Pipeline.h"
+//#include "vulkan_api/Framebuffer.h"
+//
+//#include "vulkan_api/VertexBuffer.h"
+//
+//#include "vulkan_api/CommandPool.h"
+//#include "vulkan_api/CommandBuffer.h"
+//
+//#include "vulkan_api/synchronization/Fence.h"
+//#include "vulkan_api/synchronization/Semaphore.h"
+//#include "vulkan_api/synchronization/Presenter.h"
+//
+//#include "Application.h"
--- source/platform/Surface.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-
-void Surface::Destroy(const VkInstance& instance)
-{
-	vkDestroySurfaceKHR(instance, surface, nullptr);
-}
